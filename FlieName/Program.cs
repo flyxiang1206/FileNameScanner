36ec057ae2208e48127fdd0381c77de00c6398a0
flyxiang <dinosaur860504@gmail.com>
2020-05-21 14:34:30 +0800

    First Edition

---
+++ FlieName/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FileNameScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = "";
+                string extension1 = "";
+                string extension2 = "";
+
+                #region 檢查並處理Input
+                if (args.Length != 5)
+                {
+                    Console.WriteLine("Input Type Error");
+                    Console.WriteLine("Program Return...");
+                    return;
+                }
+
+                switch (args[0])
+                {
+                    case "-path":
+                        if (args[2] != "-name")
+                        {
+                            Console.WriteLine("Input Type Error");
+                            Console.WriteLine("Program Return...");
+                            return;
+                        }
+                        path = args[1];
+                        extension1 = args[3];
+                        extension2 = args[4];
+                        break;
+                    case "-name":
+                        if (args[3] != "-path")
+                        {
+                            Console.WriteLine("Input Type Error");
+                            Console.WriteLine("Program Return...");
+                            return;
+                        }
+                        path = args[4];
+                        extension1 = args[1];
+                        extension2 = args[2];
+                        break;
+                    default:
+                        Console.WriteLine("Input Type Error");
+                        Console.WriteLine("Program Return...");
+                        return;
+                }
+
+                if (path == String.Empty || extension1 == String.Empty || extension2 == String.Empty || !Directory.Exists(path))
+                {
+                    Console.WriteLine("Path Error");
+                    Console.WriteLine("Program Return...");
+                    return;
+                }
+                #endregion
+
+                Console.WriteLine($"Scanning In {path}...");
+
+                List<string> imgName = Scanner.GetFileNameByFilenameExtension(path, extension1);
+                List<string> xmlName = Scanner.GetFileNameByFilenameExtension(path, extension2);
+
+                var extraImg = imgName.Except(xmlName).ToList();
+                var extraXml = xmlName.Except(imgName).ToList();
+
+                if (extraXml.Count == 0 && extraImg.Count == 0)
+                {
+                    Console.WriteLine("No Search any Redundant File...");
+                    Console.WriteLine($"Total File Count : [{imgName.Count},{xmlName.Count}]");
+                    Console.WriteLine("Program Return...");
+                    return;
+                }
+
+                Console.WriteLine("Redundant File Name:");
+                extraImg.ForEach(c => Console.Write(c + $".{extension1} "));
+                extraXml.ForEach(c => Console.Write(c + $".{extension2} "));
+                Console.Write("\nDelete Redundant File? (y/n)  ");
+                var key = Console.ReadKey().KeyChar;
+
+                string k = key.ToString();
+
+                if (k == "y")
+                {
+                    Console.WriteLine("\nDelete Redundant File ....");
+
+                    foreach (string s in extraImg)
+                    {
+                        System.IO.File.Delete(@$"{s}.{extension1}");
+                    }
+
+                    foreach (string s in extraXml)
+                    {
+                        System.IO.File.Delete(@$"{s}.{extension2}");
+                    }
+
+                    Console.Write("Delete Finish ....");
+                }
+
+                Console.WriteLine("\nProgram Return...");
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ FlieName/Scanner.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileNameScanner
+{
+    public class Scanner
+    {
+        /// <summary>
+        /// 取得目錄底下所有指定副檔名的檔案名稱(不包含副檔名)
+        /// </summary>
+        /// <param name="path">目標路徑</param>
+        /// <param name="extension">目標副檔名</param>
+        /// <returns></returns>
+        public static List<string> GetFileNameByFilenameExtension(string path, string extension)
+        {
+            List<string> result = new List<string>();
+            try
+            {
+                //取得當前目錄檔案
+                foreach (string name in Directory.GetFiles(path))
+                {
+                    var nameSplit = name.Split(".");
+                    var realName = "";
+
+                    if (nameSplit[nameSplit.Length - 1] == extension)
+                    {
+                        //檔名重組
+                        for (int i = 0; i < nameSplit.Length - 1; i++)
+                        {
+                            if (i == 0)
+                                realName += nameSplit[i];
+                            else
+                                realName += "." + nameSplit[i];
+                        }
+
+                        result.Add(realName);
+                    }
+                }
+
+                //取得當前根目錄檔案
+                foreach (string d in Directory.GetDirectories(path))
+                {
+                    result.AddRange(GetFileNameByFilenameExtension(d, extension));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            return result;
+        }
+    }
+}
